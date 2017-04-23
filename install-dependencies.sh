c55754418d514ac3d7052b8f8a1ed4230c78f9a3
Jiri Prokop <jprokop@synaptiko.cz>
2017-04-23 20:22:30 +0200

    install-dependencies.sh improved

---
+++ install-dependencies.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if ! pacman -Qs pacaur >& /dev/null; then
+	(>&2 echo "Pacaur is required; install it first")
+	exit 1
+fi
+
+for arg in $@; do
+	case "$arg" in
+		--dry-run|-d)
+			dryRun=true
+			;;
+		--only-missing|-o)
+			onlyMissing=true
+			;;
+	esac
+done
+
+pacmanDeps=`cat $DIR/*/dependencies | sort | uniq | grep -v '^aur/' | sort | xargs`
+pacaurDeps=`cat $DIR/*/dependencies | sort | uniq | grep '^aur/' | cut -d'/' -f2 | grep -E -v '^(pacaur|cower)$' | sort | xargs`
+
+if [ "$onlyMissing" = true ]; then
+	pacmanDeps=`pacman -T $pacmanDeps | xargs`
+	pacaurDeps=`pacman -T $pacaurDeps | xargs`
+fi
+
+if [ "$pacmanDeps" != "" ]; then
+	echo -e "\e[1msudo pacman -S \e[0m$pacmanDeps"
+	if [ "$dryRun" != true ]; then
+		sudo pacman -S $pacmanDeps
+	fi
+fi
+
+if [ "$pacaurDeps" != "" ]; then
+	echo -e "\e[1mpacaur -Sa \e[0m$pacaurDeps"
+	if [ "$dryRun" != true ]; then
+		pacaur -Sa $pacaurDeps
+	fi
+fi
