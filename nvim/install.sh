d8e546ae82e749b7d13b6df4535385c01c965cbc
Jiri Prokop <jprokop@synaptiko.cz>
2017-06-19 07:22:24 +0200

    Nvim plug.vim installation path fixed

--- nvim/install.sh
+++ nvim/install.sh
@@ -13,8 +13,8 @@ NVIM_CONFIG=$NVIM_CONFIG_DIR/init.vim
 ln -s -f $DIR/init.vim $NVIM_CONFIG
 
 if [ ! -d $NVIM_CONFIG_DIR/plugged ]; then
-	mkdir -p $NVIM_DIR/{autoload,plugged}
-	curl -sfLo $NVIM_DIR/autoload/plug.vim \
+	mkdir -p $NVIM_CONFIG_DIR/{autoload,plugged}
+	curl -sfLo $NVIM_CONFIG_DIR/autoload/plug.vim \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	pip install --user neovim
 
