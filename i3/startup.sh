36fc961efe488bd414c93508c07ec9a9e28452c0
Jiří Prokop <jprokop@synaptiko.cz>
2017-08-08 06:31:44 +0200

    Remove compton & transparency from i3/polybar/xfce-terminal

--- i3/startup.sh
+++ i3/startup.sh
@@ -3,7 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Disable DPMS and prevent screen from blanking
 xset s off -dpms
-compton -b --config $DIR/compton.config
 nitrogen --restore
 redshift >& /dev/null &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 >& /dev/null &
