b7a441ffcca361b46b115ae1d6b6435b505a6e06
Jiří Prokop <jprokop@synaptiko.cz>
2017-09-08 10:53:45 +0200

    Added xorg touchpad configuration

--- i3/install.sh
+++ i3/install.sh
@@ -10,6 +10,9 @@ ln -s -f $DIR/zlogin $ZLOGIN_CONFIG
 XORG_KEYBOARD_CONF_FILE=/etc/X11/xorg.conf.d/00-keyboard.conf
 sudo ln -s -f $DIR/xorg.conf.d-00-keyboard.conf $XORG_KEYBOARD_CONF_FILE
 
+XORG_TOUCHPAD_CONF_FILE=/etc/X11/xorg.conf.d/30-touchpad.conf
+sudo ln -s -f $DIR/xorg.conf.d-30-touchpad.conf $XORG_TOUCHPAD_CONF_FILE
+
 # TODO jprokop: parse it from user-dirs.dirs instead
 mkdir -p ~/{Downloads,Documents,Music,Pictures,Videos,Packages,Projects,work}
 USER_DIRS_CONFIG=~/.config/user-dirs.dirs
