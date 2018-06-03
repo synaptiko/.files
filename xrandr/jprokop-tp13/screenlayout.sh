2f34050c64da289f27b9ec5a30f1d3d82ed12533
Jiří Prokop <jprokop@synaptiko.cz>
2018-06-03 17:17:51 +0200

    Add top-down variant of screen layout

--- xrandr/jprokop-tp13/screenlayout.sh
+++ xrandr/jprokop-tp13/screenlayout.sh
@@ -2,5 +2,8 @@
 if xrandr | grep "HDMI1 disconnected" >& /dev/null; then
 	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 else
-	xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+	# Right & Left
+	# xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+	# Top & Down
+	xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal
 fi
