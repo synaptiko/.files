fbf906439ca08082f7d18f761b4ad2f6d066f933
Jiří Prokop <jprokop@synaptiko.cz>
2018-06-17 17:05:35 +0200

    Add initial sway configuration

---
+++ sway/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ZLOGIN_CONFIG=~/.zlogin
+ln -s -f $DIR/zlogin $ZLOGIN_CONFIG
+
+# TODO jprokop: parse it from user-dirs.dirs instead
+mkdir -p ~/{Downloads,Documents,Music,Pictures,Videos,Packages,Projects,work}
+USER_DIRS_CONFIG=~/.config/user-dirs.dirs
+ln -s -f $DIR/user-dirs.dirs $USER_DIRS_CONFIG
+xdg-user-dirs-update
+
+SWAY_CONFIG_DIR=~/.config/sway
+mkdir -p $SWAY_CONFIG_DIR
+SWAY_CONFIG=$SWAY_CONFIG_DIR/config
+ln -s -f $DIR/config $SWAY_CONFIG
