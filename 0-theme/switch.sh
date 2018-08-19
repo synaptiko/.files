a3070a38b599f1348f5e7dd1a7b3ab293c7f4f94
Jiří Prokop <jprokop@synaptiko.cz>
2018-08-19 19:38:09 +0200

    Add toggle functionality and add key binding to i3/sway

--- 0-theme/switch.sh
+++ 0-theme/switch.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/switch-alacritty.js $1 &
-$DIR/switch-nvim.sh $1 &
+ACTIVE_THEME_FILE=$DIR/configs/active-theme
+
+if [[ "$1" == "toggle" ]]; then
+	if [ -e $ACTIVE_THEME_FILE ]; then
+		theme=`cat $ACTIVE_THEME_FILE`
+		if [[ "$theme" == "light" ]]; then
+			theme=dark
+		else
+			theme=light
+		fi
+	else
+		theme=dark
+	fi
+else
+	theme=${1:-dark}
+fi
+
+echo -n $theme > $ACTIVE_THEME_FILE
+
+$DIR/switch-alacritty.js $theme &
+$DIR/switch-nvim.sh $theme &
 
 # This is ugly way how to overcome the inability of xfce4-terminal to detect changes in the symlinked file
-$DIR/switch-xfce4-terminal.js $1 && cp $DIR/configs/xfce4-terminal-terminalrc `ls -l ~/.files/0-theme/configs/link-to-xfce4-terminal-terminalrc | sed -e 's/.\+-> //'` &
+$DIR/switch-xfce4-terminal.js $theme && sleep 0.1 && cp $DIR/configs/xfce4-terminal-terminalrc `ls -l ~/.files/0-theme/configs/link-to-xfce4-terminal-terminalrc | sed -e 's/.\+-> //'` &
