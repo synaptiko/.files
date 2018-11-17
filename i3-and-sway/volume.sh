8bb920681b231de4d64f0de042dc5ba8f1745d5d
Jiří Prokop <jprokop@synaptiko.cz>
2018-11-17 19:42:35 +0100

    Add volume integration with desktop-status bus

---
+++ i3-and-sway/volume.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+case $1 in
+	toggle)
+		echo '{"volume":true,"user":"'$USER'","toggleMuted":true}' | socat unix-connect:/run/synaptiko-desktop-status-bus.socket stdio
+		;;
+	*)
+		echo '{"volume":true,"user":"'$USER'","updateVolume":'$1'}' | socat unix-connect:/run/synaptiko-desktop-status-bus.socket stdio
+		;;
+esac
