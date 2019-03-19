7cc17105616c97317d2818023bd28769165908aa
Jiří Prokop <jprokop@synaptiko.cz>
2019-03-19 21:35:50 +0100

    Fix sway background after config reload

--- sway/bg.sh
+++ sway/bg.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
-ls ~/Pictures/wallpapers | sort -R | head -1 | while read file; do
+if [ "$1" = "keep" ]; then
+	file=$(</tmp/.sway-current-bg)
 	swaymsg "output * bg ~/Pictures/wallpapers/$file fill"
-done
+else
+	ls ~/Pictures/wallpapers | sort -R | head -1 | while read file; do
+		swaymsg "output * bg ~/Pictures/wallpapers/$file fill"
+		echo $file > /tmp/.sway-current-bg
+	done
+fi
