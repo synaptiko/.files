0b598517e7fa2f1dc05b22a1a05ffd587722369e
Jiří Prokop <jprokop@synaptiko.cz>
2019-03-20 21:28:23 +0100

    Fix generating and keeping background

--- sway/bg.sh
+++ sway/bg.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 if [ "$1" = "keep" ]; then
-	file=$(</tmp/.sway-current-bg)
+	file=$(</tmp/.sway-current-bg-$USER)
 	swaymsg "output * bg ~/Pictures/wallpapers/$file fill"
 else
 	ls ~/Pictures/wallpapers | sort -R | head -1 | while read file; do
 		swaymsg "output * bg ~/Pictures/wallpapers/$file fill"
-		echo $file > /tmp/.sway-current-bg
+		echo $file > /tmp/.sway-current-bg-$USER
 	done
 fi
