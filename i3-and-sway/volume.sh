e1db4dab7dc425fe0c2d63b949d5369b90acf6e2
Jiří Prokop <jprokop@synaptiko.cz>
2019-03-20 21:18:33 +0100

    Add key bindings for low and high volume settings

--- i3-and-sway/volume.sh
+++ i3-and-sway/volume.sh
@@ -3,6 +3,9 @@ case $1 in
 	toggle)
 		echo '{"volume":true,"user":"'$USER'","toggleMuted":true}' | socat unix-connect:/run/synaptiko-desktop-status-bus.socket stdio
 		;;
+	set)
+		echo '{"volume":true,"user":"'$USER'","setVolume":'$2'}' | socat unix-connect:/run/synaptiko-desktop-status-bus.socket stdio
+		;;
 	*)
 		echo '{"volume":true,"user":"'$USER'","updateVolume":'$1'}' | socat unix-connect:/run/synaptiko-desktop-status-bus.socket stdio
 		;;
