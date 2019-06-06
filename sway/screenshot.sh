2101bc31e9c3c557b454aaf3e305b8ec1a57051e
Jiří Prokop <jprokop@synaptiko.cz>
2019-06-06 18:17:01 +0200

    Improve screenshoting with new slurp capabilities

--- sway/screenshot.sh
+++ sway/screenshot.sh
@@ -3,7 +3,13 @@
 FILE=~/Pictures/Screenshot-$(date -Iseconds | cut -d'+' -f1).png
 
 case "$1" in
-	selection) slurp -b "#45858820" -c "#45858880" -w 3 -d | grim -g - $FILE;;
+	selection)
+		windowGeometries=$(swaymsg -t get_tree \
+			| jq -r '.. | (.nodes? // empty)[] | select(.pid and .visible) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'
+		)
+		geometry=$(slurp -b "#45858820" -c "#45858880" -w 3 -d <<< "$windowGeometries") || exit $?
+		grim -g "$geometry" $FILE
+		;;
 	screen) grim $FILE;;
 esac
 
