82e3c92782dff82ff9f5739b21c69a75ef18d517
Jiří Prokop <jprokop@synaptiko.cz>
2020-03-07 10:33:36 +0100

    Fix problem with caching in wofi

--- wofi/menus.sh
+++ wofi/menus.sh
@@ -6,18 +6,18 @@ if [ "$1" == "generate-cache" ]; then
 elif [ "$1" == "programs" ]; then
 	wofi --show=drun --width=400 --lines=5
 elif [ "$1" == "browsers" ]; then
-	echo > ~/.cache/wofi-browsers
+	rm -f ~/.cache/wofi-browsers
 	LINES=`chromium-runner-rofi | wc -l`
-	ITEM=`chromium-runner-rofi | wofi --cache-file=~/.cache/wofi-browsers --dmenu --width=200 --lines=$LINES`
+	ITEM=`chromium-runner-rofi | wofi --cache-file=$HOME/.cache/wofi-browsers --dmenu --width=200 --lines=$LINES`
 	if [ $? == 0 ]; then
 		chromium-runner-rofi $ITEM
 	fi
 elif [ "$1" == "tasks" ]; then
-	echo > ~/.cache/wofi-tasks
+	rm -f ~/.cache/wofi-tasks
 	LINES=`~/Documents/todo/track-rofi.sh | wc -l`
-	ITEM=`~/Documents/todo/track-rofi.sh | wofi --cache-file=~/.cache/wofi-tasks --dmenu --width=200 --lines=$LINES`
+	ITEM=`~/Documents/todo/track-rofi.sh | wofi --cache-file=$HOME/.cache/wofi-tasks --dmenu --width=200 --lines=$LINES`
 	if [ $? == 0 ]; then
-		~/Documents/todo/track-rofi.sh $ITEM
+		~/Documents/todo/track-rofi.sh "$ITEM"
 	fi
 elif [ "$1" == "close" ]; then
 	pkill wofi
