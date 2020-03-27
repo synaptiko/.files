b4e8348fdfbe078f4f40e8c8578f2c146381ef00
Jiří Prokop <jprokop@synaptiko.cz>
2020-03-27 21:33:09 +0100

    Start using synaptiko-browser-profile-runner instead of chromium-runner

--- wofi/menus.sh
+++ wofi/menus.sh
@@ -2,15 +2,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$1" == "generate-cache" ]; then
-	chromium-runner --generate-cache
+	synaptiko-browser-profile-runner --generate-cache
 elif [ "$1" == "programs" ]; then
 	wofi --show=drun --width=400 --lines=5
 elif [ "$1" == "browsers" ]; then
 	rm -f ~/.cache/wofi-browsers
-	LINES=`chromium-runner-rofi | wc -l`
-	ITEM=`chromium-runner-rofi | wofi --cache-file=$HOME/.cache/wofi-browsers --dmenu --width=200 --lines=$LINES`
+	LINES=`synaptiko-browser-profile-runner-rofi | wc -l`
+	ITEM=`synaptiko-browser-profile-runner-rofi | wofi --cache-file=$HOME/.cache/wofi-browsers --dmenu --width=200 --lines=$LINES`
 	if [ $? == 0 ]; then
-		chromium-runner-rofi $ITEM
+		synaptiko-browser-profile-runner-rofi $ITEM
 	fi
 elif [ "$1" == "tasks" ]; then
 	rm -f ~/.cache/wofi-tasks
