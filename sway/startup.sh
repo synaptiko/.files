b64b6c41cdc38d2f59ce85b5f00ac56c037ce201
Jiří Prokop <jprokop@synaptiko.cz>
2020-11-18 08:48:15 +0100

    Add gnome-shell-screenshot-dbus-emulator

--- sway/startup.sh
+++ sway/startup.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+systemctl --user import-environment
+
 gammastep -m wayland -r >& /dev/null &
 
 mako >& /dev/null &
 
 kanshi >& /dev/null &
 
-# TODO jprokop: polkit-gnome sucks but polkit-mate segfaults under sway/wayland :-( (revisit later)
+# TODO jprokop: polkit-gnome sucks but polkit-mate segfaults under sway/wayland :-( (revisit later; revisited 11/2020 and was still happening)
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >& /dev/null &
 
-# TODO jprokop: this used to work automatically but it's not anymore (investigate later if it's still needed or not)
-PIPEWIRE_DEBUG=3 pipewire >& /dev/null &
-/usr/lib/xdg-desktop-portal -r >& /dev/null &
-/usr/lib/xdg-desktop-portal-wlr >& /dev/null &
+$DIR/../wayland-screen-share/gnome-shell-screenshot-dbus-emulator >& /dev/null &
