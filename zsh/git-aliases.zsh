d9cbd91570851fcfab3ca0d082efe7a56d2125be
Jiří Prokop <jprokop@synaptiko.cz>
2021-04-21 07:55:48 +0200

    Improve gsync function

--- zsh/git-aliases.zsh
+++ zsh/git-aliases.zsh
@@ -59,15 +59,32 @@ gbackup() {
 }
 
 gsync() {
-	if git branch --show-current | grep -E "^master$" > /dev/null ; then
-		# TODO jprokop: add also check that upstream remote exists and that the current master is origin
-		# TODO jprokop: detect deleted branches from the following two commands
-		set -x
-		git fetch --prune
-		git fetch --prune upstream
-		git rebase upstream/master
-		git push
-		set +x
+	if git branch --show-current | grep -E "^master$" > /dev/null; then
+		if git remote -v | grep -E "^upstream\s+" > /dev/null; then
+			if git rev-parse --abbrev-ref HEAD@{upstream} | grep -E "^origin/master$" > /dev/null; then
+				fetchResult=$(mktemp)
+				echo "git fetch --prune"
+				git fetch --prune 2>&1 | tee $fetchResult
+				echo "git fetch --prune upstream"
+				git fetch --prune upstream 2>&1 | tee -a $fetchResult
+				echo "git rebase upstream/master"
+				git rebase upstream/master
+				echo "git push"
+				git push
+
+				echo
+				branchesToDelete=$(mktemp)
+				# TODO jprokop: improve later, somehow detect that branches are related to the remote and also that they do not contain unpushed changes
+				sed -e "/^ - \[deleted\]/!d" $fetchResult | sed -e "s/^.*\/\([^/]*\)$/\1/" | sort | uniq > $branchesToDelete
+				cat $branchesToDelete | xargs -p git branch -D
+
+				rm $branchesToDelete $fetchResult
+			else
+				echo "This branch doesn't track origin remote; gsync currently doesn't support this."
+			fi
+		else
+			echo "This repository doesn't have upstream remote defined; gsync currently doesn't support this."
+		fi
 	else
 		echo "Not on master branch; gsync currently doesn't support this."
 	fi
