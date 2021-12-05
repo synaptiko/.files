54245aae832e39e54eed6afc9382250ab462aa8c
Jiří Prokop <jiri@bayzat.com>
2021-12-05 11:12:20 +0100

    Improve moving windows between displays

--- sway/move-to-output--temporary-fix.sh
+++ sway/move-to-output--temporary-fix.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 workspace=`swaymsg -t get_tree | jq '.nodes[] | select(.type == "output") | select(.name == "'$1'") | .current_workspace'`
 swaymsg move container to workspace $workspace
+swaymsg workspace $workspace
+
+~/.files/sway/switch-output.sh $2
